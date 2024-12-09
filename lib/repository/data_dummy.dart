c113fed2ea84a43cd45775bf4348e8b651a9d5c6
Davine-Dap <davinechen100@gmail.com>
2024-12-09 19:39:17 +0700

    Add files via upload

---
+++ lib/repository/data_dummy.dart
@@ -0,0 +1,66 @@
+import 'package:latihan_7/models/user_models.dart';
+
+class DataDummy {
+  List<UserModel> listUser = [
+    UserModel(
+        id: 1,
+        firstName: 'Emily',
+        lastName: 'Johnson',
+        gender: 'Female',
+        image: 'https://dummyjson.com/icon/emilys/128'),
+    UserModel(
+        id: 2,
+        firstName: 'Michael',
+        lastName: 'Michael',
+        gender: 'Male',
+        image: 'https://dummyjson.com/icon/michaelw/128'),
+    UserModel(
+        id: 3,
+        firstName: 'Sophia',
+        lastName: 'Brown',
+        gender: 'Female',
+        image: 'https://dummyjson.com/icon/sophiab/128'),
+    UserModel(
+        id: 4,
+        firstName: 'James',
+        lastName: 'Davis',
+        gender: 'Male',
+        image: 'https://dummyjson.com/icon/jamesd/128'),
+    UserModel(
+        id: 5,
+        firstName: 'Emma',
+        lastName: 'Miller',
+        gender: 'Female',
+        image: 'https://dummyjson.com/icon/emmaj/128'),
+    UserModel(
+        id: 6,
+        firstName: 'Olivia',
+        lastName: 'Wilson',
+        gender: 'Female',
+        image: 'https://dummyjson.com/icon/oliviaw/128'),
+    UserModel(
+        id: 7,
+        firstName: 'Alexander',
+        lastName: 'Jones',
+        gender: 'Male',
+        image: 'https://dummyjson.com/icon/alexanderj/128'),
+    UserModel(
+        id: 8,
+        firstName: 'Ava',
+        lastName: 'Taylor',
+        gender: 'Female',
+        image: 'https://dummyjson.com/icon/avat/128'),
+    UserModel(
+        id: 9,
+        firstName: 'Ethan',
+        lastName: 'Martinez',
+        gender: 'Male',
+        image: 'https://dummyjson.com/icon/ethanm/128'),
+    UserModel(
+        id: 10,
+        firstName: 'Isabella',
+        lastName: 'Anderson',
+        gender: 'Female',
+        image: 'https://dummyjson.com/icon/isabellad/128'),
+  ];
+}
